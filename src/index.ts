af31f1ae634f16f4eaf5e474661a6afa16416e1e
JAMES FUQIAN <jfuqian@ellumen.com>
2022-03-02 20:30:35 -0800

    changes per feedback

--- src/index.ts
+++ src/index.ts
@@ -55,15 +55,14 @@ export default class BlueButton {
       throw new Error("clientSecret is required");
     }
 
-    // if (!bbJsonConfig.callbackUrl) {
-    //   throw new Error("callbackUrl is required");
-    // }
+    if (!bbJsonConfig.callbackUrl) {
+      throw new Error("callbackUrl is required");
+    }
 
     this.baseUrl = bbJsonConfig.baseUrl;
     this.clientId = bbJsonConfig.clientId;
     this.callbackUrl = bbJsonConfig.callbackUrl;
     this.clientSecret = bbJsonConfig.clientSecret;
-    this.callbackUrl = bbJsonConfig.callbackUrl;
     this.version = bbJsonConfig.version;
   }
 
