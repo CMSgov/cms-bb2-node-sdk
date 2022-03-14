0fe344828e240c4202e7cff6f1a296b54346d9f8
David Tisza <36048547+dtisza1@users.noreply.github.com>
2022-03-14 16:22:56 -0400

    [ BB2-1092 ] Create auth flow methods (#3)
    * Create PKCE utility functions
    
    * Create init AUTH and tests for BB class
    
    * Refactor per github review comments
    
    * Refactor methods from external auth.ts in class
    
    * Update generateAuthData() in index.ts
    
    * Add method for generating token post data
    
    - Move types in auth.ts to types/auth.ts
    
    - Create method `generateTokenPostData()` taking callback code
      and state inputs to generate post data for token endpoint call.
    
    - Create tests for `generateTokenPostData()` return and state does not match exception.
    
    BB2-1092
    
    * Update comments in auth.ts
    
    * Update w/ misc cleanup
    
    * Add method to validate call back params
    
    - Add `validateCallBackRequestQueryParams()` function in `src/auth.ts`
    
    - Add `validateCallBackRequestQueryParams()` function as a method in `src/auth.ts`
    
    - Create `AuthorizationToken` class in `src/entities/auth.ts`.
    
    - Create error message ENUMs in `
    
    - Add tests for `validateCallBackRequestQueryParams()` method in `src/auth.test.ts`
    
    - Update `index.test.ts` for `callBackUrl`
    
    BB2-1092
    
    * Change class/type namings to be Capitalized
    
    * Refactor per PR review
    
    * Refactor namings for merge w/ main
    
    * Rename AuthorizationToken to AccessToken
    
    * Rename CallBack to Callback
    
    * Create initial getAccessToken() method
    
    * Create get authorization token functionality
    
    - Add `moment` and `axios` to dependencies in `package.json`
    
    - Change `AccessToken` to `AuthorizationToken` class and refactor attribute namings in `src/entities/AuthorizationToken.ts`
    
    - Create `getAuthorizationToken()` async function in `src/auth.ts`.
    
    - Create `getAuthorizationToken()` public async method for `BlueButton` class `in `src/index.ts` that utilizes underlying async function
    
    - Add tests related to get authorization token functionality in `src/auth.test.ts`
    
    - Add `yarn.lock` to `.gitignore` file.
    
    BB2-1092
    
    * Fix string array type for AuthorizationToken
    
    * Create getAccessTokenUrl() method
    
    * Create getAuthorizationUrl() method
    
    * Add token endpoint response execptions
    
    - Add `AUTH_TOKEN_URL_*` error enums to `src/enums/errors.ts`
    
    - Add private `_authResponseStatusCode` to `BlueButton` class
      with `getAuthResponseStatusCode()` and `setAuthResponseStatusCode()`
       methods in `src/index.ts`
    
    - Add token endpoint response code exceptions for
      `getAuthorizationToken()` function in `src/auth.ts`
    
    - Add tests for token endpoint response code exceptions
      of `getAuthorizationToken()` method in `src/auth.test.ts`
    
    BB2-1092
    
    * Update per PR review comments and more
    
    - Change some namings related to camel case convention
    
    - Add `setAuthResponseStatusCode(-1)` prior to token endpoint call in `src/auth.ts`
    
    - Add tests to validate responses in `src/auth.tests.ts`.
    
    BB2-1092
    
    * Update per PR review change request
    
    * Update per PR review change request
    
    * Remove Axios response error handling and tests
    
    * Fix moment expires time for milliseconds
    
    * Fix expires_at test
    
    * Move getAccessTokenUrl() to auth.ts as function
    
    * Move function getAuthorizationUrl() to auth.tx
    
    * Remove comments

---
+++ src/auth.ts
@@ -0,0 +1,150 @@
+/*
+auth.ts - Provides auth related methods for the Bluebutton class
+*/
+import axios from "axios";
+import crypto from "crypto";
+
+import BlueButton from ".";
+import { AuthorizationToken } from "./entities/AuthorizationToken";
+import { Errors } from "./enums/errors";
+
+type PkceData = {
+  codeChallenge: string;
+  verifier: string;
+};
+
+function base64URLEncode(buffer: Buffer): string {
+  return buffer
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=/g, "");
+}
+
+function sha256(str: string): Buffer {
+  return crypto.createHash("sha256").update(str).digest();
+}
+
+function generatePkceData(): PkceData {
+  var verifier = base64URLEncode(crypto.randomBytes(32));
+  return {
+    codeChallenge: base64URLEncode(sha256(verifier)),
+    verifier: verifier,
+  };
+}
+
+function generateRandomState(): string {
+  return base64URLEncode(crypto.randomBytes(32));
+}
+
+export type AuthData = {
+  codeChallenge: string;
+  verifier: string;
+  state: string;
+};
+
+export type TokenPostData = {
+  client_id: string;
+  client_secret: string;
+  code?: string;
+  grant_type: string;
+  redirect_uri: string;
+  code_verifier: string;
+  code_challenge: string;
+};
+
+export function generateAuthData(): AuthData {
+  const PkceData = generatePkceData();
+  return {
+    codeChallenge: PkceData.codeChallenge,
+    verifier: PkceData.verifier,
+    state: generateRandomState(),
+  };
+}
+
+function getAuthorizationUrl(bb: BlueButton): string {
+  return `${bb.baseUrl}/v${bb.version}/o/authorize`;
+}
+
+export function generateAuthorizeUrl(
+  bb: BlueButton,
+  AuthData: AuthData
+): string {
+  const pkceParams = `code_challenge_method=S256&code_challenge=${AuthData.codeChallenge}`;
+
+  return `${getAuthorizationUrl(bb)}?client_id=${bb.clientId}&redirect_uri=${
+    bb.callbackUrl
+  }&state=${AuthData.state}&response_type=code&${pkceParams}`;
+}
+
+//  Generates post data for call to access token URL
+export function generateTokenPostData(
+  bb: BlueButton,
+  authData: AuthData,
+  callbackCode?: string
+): TokenPostData {
+  return {
+    client_id: bb.clientId,
+    client_secret: bb.clientSecret,
+    code: callbackCode,
+    grant_type: "authorization_code",
+    redirect_uri: bb.callbackUrl,
+    code_verifier: authData.verifier,
+    code_challenge: authData.codeChallenge,
+  };
+}
+
+function validateCallbackRequestQueryParams(
+  AuthData: AuthData,
+  callbackCode?: string,
+  callbackState?: string,
+  callbackError?: string
+) {
+  // Check state from callback here?
+  if (callbackError === "access_denied") {
+    throw new Error(Errors.CALLBACK_ACCESS_DENIED);
+  }
+
+  if (!callbackCode) {
+    throw new Error(Errors.CALLBACK_ACCESS_CODE_MISSING);
+  }
+
+  if (!callbackState) {
+    throw new Error(Errors.CALLBACK_STATE_MISSING);
+  }
+
+  if (callbackState != AuthData.state) {
+    throw new Error(Errors.CALLBACK_STATE_DOES_NOT_MATCH);
+  }
+}
+
+function getAccessTokenUrl(bb: BlueButton): string {
+  return `${bb.baseUrl}/v${bb.version}/o/token/`;
+}
+
+// Get an access token from callback code & state
+export async function getAuthorizationToken(
+  bb: BlueButton,
+  AuthData: AuthData,
+  callbackRequestCode?: string,
+  callbackRequestState?: string,
+  callbackRequestError?: string
+) {
+  validateCallbackRequestQueryParams(
+    AuthData,
+    callbackRequestCode,
+    callbackRequestState,
+    callbackRequestError
+  );
+
+  const postData = generateTokenPostData(bb, AuthData, callbackRequestCode);
+
+  const resp = await axios.post(getAccessTokenUrl(bb), postData);
+
+  if (resp.data) {
+    const authToken = new AuthorizationToken(resp.data);
+    return authToken;
+  } else {
+    throw Error(Errors.AUTH_TOKEN_URL_RESPONSE_DATA_MISSING);
+  }
+}
